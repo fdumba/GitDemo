d687d149bf2f63f96830a9d5897bd351a2cb9ba1
fdumba <eziz2118@gmail.com>
2020-04-19 12:24:00 -0400

    simple changes

--- src/test/java/ezizprojects/RESTAPITest.java
+++ src/test/java/ezizprojects/RESTAPITest.java
@@ -7,6 +7,9 @@ public class RESTAPITest {
 	@Test
 	public void deleteTwitter() {
 		System.out.println("BrowserAutomation");
+		System.out.println("BrowserAutomation1");
+		System.out.println("BrowserAutomation2");
+		System.out.println("BrowserAutomation3");
 	}
 
 	@Test
